a9aaac78ea6c1ab0580001b97e31ce938b446d96
Thamires Pieve <thamires.pieve@gec.inatel.br>
2022-03-01 17:19:24 -0300

    feat: Actions menu button

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 // DOM- Document Object Model
+/* Abre e fecha o menu quando clicar no icone */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+// Para cada elemento de toggle
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* Quando clicar em um item do menu, esconder o menu */
+/* Pegou os seletores da lista */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const element of links) {
+  element.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* Mudar o header da pagina quando der Scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (this.window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
